cf48f0b3de16fd968b0abb118c8f8169acf2ccfd
Yi Xun <yixun89@live.com>
2024-05-27 21:04:18 +0800

    new div added in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+    <div>
       <h1>Currency Converter</h1>
       <CurrencyDropdown />
+      </div>
     </>
   );
 }
