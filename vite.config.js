850b1ec82a7bfbf3821789131b88fc9ab212edb2
Yi Xun <yixun89@live.com>
2024-05-27 21:27:00 +0800

    added workflow, modified

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/currency-converter',
+  test:{
+   globals:true,
+   environment:'happy-dom',
+   setupFiles:'src/setupTests.js'
+  }
 })
